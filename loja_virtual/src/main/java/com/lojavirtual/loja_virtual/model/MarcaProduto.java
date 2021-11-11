8b7344d719694394e4a7ad32693ff35494833b8f
americo afonso <afonsoamericoafonso@gmail.com>
2021-11-11 15:39:13 -0300

    Implementacao da entidade MarcaProduto

---
+++ loja_virtual/src/main/java/com/lojavirtual/loja_virtual/model/MarcaProduto.java
@@ -0,0 +1,22 @@
+package com.lojavirtual.loja_virtual.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private long id;
+
+    private String nomeDesc;
+}
