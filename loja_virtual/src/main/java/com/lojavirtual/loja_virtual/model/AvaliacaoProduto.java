a9d59370fab9775f8ac70cb65af1f64161c96fd5
americo afonso <afonsoamericoafonso@gmail.com>
2022-01-19 18:09:57 -0300

    Ajuste na entidade AvaliacaoProduto.
    Ajuste na entidade AvaliacaoProduto na coluna produto_id.

--- loja_virtual/src/main/java/com/lojavirtual/loja_virtual/model/AvaliacaoProduto.java
+++ loja_virtual/src/main/java/com/lojavirtual/loja_virtual/model/AvaliacaoProduto.java
@@ -28,6 +28,7 @@ public class AvaliacaoProduto implements Serializable {
             foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
     private Pessoa pessoa;
 
+    @ManyToOne(targetEntity = Produto.class)
     @JoinColumn(name = "produto_id", nullable = false,
             foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
     private Produto produto;
