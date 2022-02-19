4835da6af1c295c7c63ff18daa05a27c91d61828
americoafonso <americo.quim@hotmail.com>
2022-02-19 19:15:15 -0300

    Implementacao do metodo delelete.

--- loja_virtual/src/test/java/com/lojavirtual/loja_virtual/LojaVirtualApplicationTests.java
+++ loja_virtual/src/test/java/com/lojavirtual/loja_virtual/LojaVirtualApplicationTests.java
@@ -2,12 +2,16 @@ package com.lojavirtual.loja_virtual;
 
 import com.lojavirtual.loja_virtual.controller.AcessoController;
 import com.lojavirtual.loja_virtual.model.Acesso;
+import com.lojavirtual.loja_virtual.repository.AcessoRepository;
+import com.lojavirtual.loja_virtual.service.AcessoService;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = LojaVirtualApplication.class)
-public class LojaVirtualApplicationTests {
+public class LojaVirtualApplicationTests extends TestCase {
 
 	@Autowired
 	private AcessoController acessoController;
@@ -16,8 +20,10 @@ public class LojaVirtualApplicationTests {
 	public void testCadastraAcesso() {
 		Acesso acesso = new Acesso();
 		acesso.setDescricao("ROLE_ADMIN");
+		acesso = acessoController.salvar(acesso).getBody();
 
-		acessoController.salvar(acesso);
+		assert acesso != null;
+		Assertions.assertNotNull(acesso.getId());
 	}
 
 }
