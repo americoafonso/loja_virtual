a316253003a116de7c39909c5cc7dd9405de771c
americoafonso <americo.quim@hotmail.com>
2022-03-24 23:30:01 -0300

    Implementacao do security e JWT parte II

--- loja_virtual/src/main/java/com/lojavirtual/loja_virtual/security/JWTTokenAutenticacaoService.java
+++ loja_virtual/src/main/java/com/lojavirtual/loja_virtual/security/JWTTokenAutenticacaoService.java
@@ -1,19 +1,26 @@
 package com.lojavirtual.loja_virtual.security;
 
+import com.lojavirtual.loja_virtual.AplicationContextLoad;
+import com.lojavirtual.loja_virtual.model.Usuario;
+import com.lojavirtual.loja_virtual.repository.UsuarioRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
-
-import static java.security.KeyRep.Type.SECRET;
 
 @Service
 public class JWTTokenAutenticacaoService {
 
     // Token de validade de 11 dias
     private static final long EXPIRATION_TIME = 959990000;
+
+    /*Chave de senha para juntar com o JWT*/
+    private static final String SECRET = "ss/-*-*sds565dsd-s/d-s*dsds";
 
     // Chave de senha para juntar com jwt
     private static final String TOKEN_PREFIX = "Bearer";
@@ -26,16 +33,64 @@ public class JWTTokenAutenticacaoService {
         String JWT = Jwts.builder() // chama o gerador de token
                 .setSubject(username) // Adiciona o user
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, String.valueOf(SECRET)).compact(); // Tempo de expiracao
+                .signWith(SignatureAlgorithm.HS512, SECRET).compact(); /*Temp de expiração*/
 
         String token = TOKEN_PREFIX + " " + JWT;
 
         // Da a resposta pra tela e para o cliente, ex: outra api, navegador, outra chamada qualquer...
         response.addHeader(HEADER_STRING, token);
 
+        liberacaoCors(response);
+
         // Usado para ver no Postman para teste
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+    }
 
+    public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+            String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+            String user = Jwts.parser().
+                    setSigningKey(SECRET)
+                    .parseClaimsJws(tokenLimpo)
+                    .getBody().getSubject(); /*ADMIN ou Americo*/
+
+            if (user != null) {
+                Usuario usuario = AplicationContextLoad
+                        .getApplicationContext()
+                        .getBean(UsuarioRepository.class).findUserByLogin(user);
+
+                if (usuario != null) {
+                    return new UsernamePasswordAuthenticationToken(
+                            usuario.getLogin(),
+                            usuario.getSenha(),
+                            usuario.getAuthorities());
+                }
+            }
+        }
+        liberacaoCors(response);
+        return null;
+    }
+
+    // Fazendo liberacao contra erro de cors no navegador
+    private void liberacaoCors(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 
 }
