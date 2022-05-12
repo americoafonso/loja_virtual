9c5d4debb0469bab1cfc29f3877cc99332b61bde
americoafonso <americo.quim@hotmail.com>
2022-05-12 00:53:24 -0300

    Implementacao do security e JWT parte II(ajuste)

--- loja_virtual/src/main/java/com/lojavirtual/loja_virtual/LojaVirtualApplication.java
+++ loja_virtual/src/main/java/com/lojavirtual/loja_virtual/LojaVirtualApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class LojaVirtualApplication {
 	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("12345"));
 		SpringApplication.run(LojaVirtualApplication.class, args);
 	}
 }
--- loja_virtual/src/main/java/com/lojavirtual/loja_virtual/security/JWTTokenAutenticacaoService.java
+++ loja_virtual/src/main/java/com/lojavirtual/loja_virtual/security/JWTTokenAutenticacaoService.java
@@ -79,15 +79,12 @@ public class JWTTokenAutenticacaoService {
         if (response.getHeader("Access-Control-Allow-Origin") == null) {
             response.addHeader("Access-Control-Allow-Origin", "*");
         }
-
         if (response.getHeader("Access-Control-Allow-Headers") == null) {
             response.addHeader("Access-Control-Allow-Headers", "*");
         }
-
         if (response.getHeader("Access-Control-Request-Headers") == null) {
             response.addHeader("Access-Control-Request-Headers", "*");
         }
-
         if (response.getHeader("Access-Control-Allow-Methods") == null) {
             response.addHeader("Access-Control-Allow-Methods", "*");
         }
